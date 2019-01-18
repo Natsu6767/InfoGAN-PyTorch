77e7c7ee50ba3df69cb0c615fdefc91e795946c5
Mohit Jain <mohitjain1999@yahoo.com>
2019-01-18 17:05:11 +0530

    Create NormalNLLLoss fucntion.

--- InfoGAN/utils.py
+++ InfoGAN/utils.py
@@ -6,3 +6,14 @@ def weights_init(m):
 	elif(type(m) == nn.BatchNorm2d):
 		nn.init.normal_(m.weight.data, 1.0, 0.02)
 		nn.init.constant_(m.bias.data, 0)
+
+def NormalNLLLoss(x, mu, var):
+	"""
+	Calculate the negative log likelihood
+	of normal distribution.
+	This needs to be minimised.
+	"""
+	logli = -0.5 * (var.mul(2 * np.pi) + 1e-6).log() - (x - mu).pow(2).div(var.mul(2.0) + 1e-6)
+	nll = -(logli.sum(1).mean())
+
+	return nll
