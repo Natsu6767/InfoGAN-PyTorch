a63aa7cb7b12690161c5f1e6e65f61a178495571
Mohit Jain <mohitjain1999@yahoo.com>
2019-01-18 19:28:42 +0530

    Update noise sampler to generate variable dimensional and type noise.

--- InfoGAN/utils.py
+++ InfoGAN/utils.py
@@ -24,13 +24,24 @@ class NormalNLLLoss:
 
         return nll
 
-def noise_sampler(dis_c, con_c, noise, batch_size):
+def noise_sampler(n_dis_c, dis_c_dim, n_con_c, n_z, batch_size, device):
 
-    idx = np.random.randint(10, size=batch_size)
-    c = np.zeros((batch_size, 10))
-    c[range(batch_size), idx] = 1.0
+    z = torch.randn(batch_size, n_z, 1, 1, device=device)
 
-    dis_c.data.copy_(torch.Tensor(c))
-    con_c.data.uniform_(-1.0, 1.0)
-    noise.data.normal_(0.0, 1.0)
+    dis_c = torch.zeros(batch_size, n_dis_c, dis_c_dim, device=device)
+    idx = np.zeros((n_dis_c, batch_size))
+    for i in range(n_dis_c):
+        idx[i] = np.random.randint(dis_c_dim, size=batch_size)
+        c[torch.arange(0, batch_size), i, idx[i]] = 1.0
 
+    dis_c.squeeze_()
+    if batch_size == 1:
+        dis_c.unsqueeze_(0)
+
+    dis_c = dis_c.view(batch_size, -1, 1, 1)
+
+    con_c = torch.Tensor(batch_size, n_con_c, 1, 1, device=device).uniform(-1, 1)
+
+    noise = torch.cat((z, dis_c, con_c), dim=1)
+
+    return noise, idx
