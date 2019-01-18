91b24b40673eaaa6218a1ec6404e3c29d1f0740a
Mohit Jain <mohitjain1999@yahoo.com>
2019-01-18 16:43:25 +0530

    Implement discriminator with D and Q of InfoGAN.

--- InfoGAN/mnist_model.py
+++ InfoGAN/mnist_model.py
@@ -1,5 +1,6 @@
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 
 class Generator(nn.Module):
 	def __init__(self):
@@ -25,3 +26,53 @@ class Generator(nn.Module):
 
 		return img
 
+class Discriminator(nn.Module):
+	def __init__(self):
+		super().__init__()
+
+		self.conv1 = nn.Conv2d(1, 64, 4, 2, 1)
+
+		self.conv2 = nn.Conv2d(64, 128, 4, 2, 1, bias=False)
+		self.bn2 = nn.BatchNorm2d(128)
+
+		self.conv3 = nn.Conv2d(128, 1024, 7, bias=False)
+		self.bn3 = nn.BatchNorm2d(1024)
+
+	def forward(self, x):
+		x = F.leaky_relu(self.conv1(x), 0.1, inplace=True)
+		x = F.leaky_relu(self.bn2(self.conv2(x)), 0.1, inplace=True)
+		x = F.leaky_relu(self.bn3(self.conv3(x)), 0.1, inplace=True)
+
+		return x
+
+class DHead(nn.Module):
+	def __init__(self):
+		super()__init__()
+
+		self.conv = nn.Conv2d(1024, 1, 1)
+
+	def forward(self, x):
+		output = torch.sigmoid(self.conv(x))
+
+		return output
+
+class QHead(nn.Module):
+	def __init__(self):
+		super().__init__()
+
+		self.conv1 = nn.Conv2d(1024, 128, 1, bias=False)
+		self.bn1 = nn.BatchNorm2d(128)
+
+		self.conv_disc = nn.Conv2d(128, 10, 1)
+		self.conv_mu = nn.Conv2d(128, 2, 1)
+		self.conv_var = nn.Conv2d(128, 2, 1)
+
+	def forward(self, x):
+		x = F.leaky_relu(self.bn1(self.conv1(x)), 0.1, inplace=True)
+
+		disc_logits = self.conv_disc(x).squeeze()
+
+		mu = self.conv_mu(x).squeeze()
+		var = torch.exp(self.conv_var(x).squeeze())
+
+		return disc_logits, mu, var
