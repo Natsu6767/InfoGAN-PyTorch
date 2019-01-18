e10b2315662f11018309e402face6333c272cf7e
Mohit Jain <mohitjain1999@yahoo.com>
2019-01-19 00:03:37 +0530

    Change indentations to spaces.

--- InfoGAN/dataloader.py
+++ InfoGAN/dataloader.py
@@ -7,45 +7,45 @@ root = 'data/'
 
 def get_data(dataset, batch_size):
 
-	if dataset == 'MNIST':
-		transform = transforms.Compose([
-			transforms.Resize(28),
-			transforms.CenterCrop(28),
-			transforms.ToTensor()])
+    if dataset == 'MNIST':
+        transform = transforms.Compose([
+            transforms.Resize(28),
+            transforms.CenterCrop(28),
+            transforms.ToTensor()])
 
-		dataset = dsets.MNIST(root, train='train', 
-			                    download=True, transform=transform)
+        dataset = dsets.MNIST(root, train='train', 
+                                download=True, transform=transform)
 
-	elif dataset == 'SVHN':
-		transform = transforms.Compose([
-			transforms.Resize(32),
-			transforms.CenterCrop(32),
-			transforms.ToTensor()])
+    elif dataset == 'SVHN':
+        transform = transforms.Compose([
+            transforms.Resize(32),
+            transforms.CenterCrop(32),
+            transforms.ToTensor()])
 
-		dataset = dsets.SVHN(root, split='train', 
-			                    download=True, transform=transform)
+        dataset = dsets.SVHN(root, split='train', 
+                                download=True, transform=transform)
 
-	elif dataset = 'FashionMNIST':
-		transform = transforms.Compose([
-			transforms.Resize(28),
-			transforms.CenterCrop(28),
-			transforms.ToTensor()])
+    elif dataset = 'FashionMNIST':
+        transform = transforms.Compose([
+            transforms.Resize(28),
+            transforms.CenterCrop(28),
+            transforms.ToTensor()])
 
-		dataset = dsets.FashionMNIST(root, train='train', 
-			                    download=True, transform=transform)
+        dataset = dsets.FashionMNIST(root, train='train', 
+                                download=True, transform=transform)
 
-	elif dataset = 'CelebA':
-		transform = transforms.Compose([
-			transforms.Resize(32),
-			transforms.CenterCrop(32),
-			transforms.ToTensor(),
-			transforms.Normalize((0.5, 0.5, 0.5),
-				(0.5, 0.5, 0.5))])
+    elif dataset = 'CelebA':
+        transform = transforms.Compose([
+            transforms.Resize(32),
+            transforms.CenterCrop(32),
+            transforms.ToTensor(),
+            transforms.Normalize((0.5, 0.5, 0.5),
+                (0.5, 0.5, 0.5))])
 
-		dataset = dsets.ImageFolder(root=root, transform=transform)
+        dataset = dsets.ImageFolder(root=root, transform=transform)
 
-	dataloader = torch.utils.data.DataLoader(dataset, 
-		                                    batch_size=batch_size, 
-		                                    shuffle=True)
+    dataloader = torch.utils.data.DataLoader(dataset, 
+                                            batch_size=batch_size, 
+                                            shuffle=True)
 
-	return dataloader
+    return dataloader
--- InfoGAN/mnist_model.py
+++ InfoGAN/mnist_model.py
@@ -3,76 +3,76 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 class Generator(nn.Module):
-	def __init__(self):
-		super().__init__()
+    def __init__(self):
+        super().__init__()
 
-		self.tconv1 = nn.ConvTransposed2d(74, 1024, 1, 1, bias=False)
-		self.bn1 = nn.BatchNorm2d(1024)
+        self.tconv1 = nn.ConvTransposed2d(74, 1024, 1, 1, bias=False)
+        self.bn1 = nn.BatchNorm2d(1024)
 
-		self.tconv2 = nn.ConvTransposed2d(1024, 128, 7, 1, bias=False)
-		self.bn2 = nn.BatchNorm2d(128)
+        self.tconv2 = nn.ConvTransposed2d(1024, 128, 7, 1, bias=False)
+        self.bn2 = nn.BatchNorm2d(128)
 
-		self.tconv3 = nn.ConvTransposed2d(128, 64, 4, 2, padding=1, bias=False)
-		self.bn3 = nn.BatchNorm2d(64)
+        self.tconv3 = nn.ConvTransposed2d(128, 64, 4, 2, padding=1, bias=False)
+        self.bn3 = nn.BatchNorm2d(64)
 
-		self.tconv4 = nn.BatchNorm2d(64, 1, 4, 2, 1, padding=1, bias=False)
+        self.tconv4 = nn.BatchNorm2d(64, 1, 4, 2, 1, padding=1, bias=False)
 
-	def forward(self, x):
-		x = torch.relu(self.bn1(self.tconv1(x)))
-		x = torch.relu(self.bn2(self.tconv2(x)))
-		x = torch.relu(self.bn3(self.tconv3(x)))
+    def forward(self, x):
+        x = torch.relu(self.bn1(self.tconv1(x)))
+        x = torch.relu(self.bn2(self.tconv2(x)))
+        x = torch.relu(self.bn3(self.tconv3(x)))
 
-		img = torch.sigmoid(self.tconv4(x))
+        img = torch.sigmoid(self.tconv4(x))
 
-		return img
+        return img
 
 class Discriminator(nn.Module):
-	def __init__(self):
-		super().__init__()
+    def __init__(self):
+        super().__init__()
 
-		self.conv1 = nn.Conv2d(1, 64, 4, 2, 1)
+        self.conv1 = nn.Conv2d(1, 64, 4, 2, 1)
 
-		self.conv2 = nn.Conv2d(64, 128, 4, 2, 1, bias=False)
-		self.bn2 = nn.BatchNorm2d(128)
+        self.conv2 = nn.Conv2d(64, 128, 4, 2, 1, bias=False)
+        self.bn2 = nn.BatchNorm2d(128)
 
-		self.conv3 = nn.Conv2d(128, 1024, 7, bias=False)
-		self.bn3 = nn.BatchNorm2d(1024)
+        self.conv3 = nn.Conv2d(128, 1024, 7, bias=False)
+        self.bn3 = nn.BatchNorm2d(1024)
 
-	def forward(self, x):
-		x = F.leaky_relu(self.conv1(x), 0.1, inplace=True)
-		x = F.leaky_relu(self.bn2(self.conv2(x)), 0.1, inplace=True)
-		x = F.leaky_relu(self.bn3(self.conv3(x)), 0.1, inplace=True)
+    def forward(self, x):
+        x = F.leaky_relu(self.conv1(x), 0.1, inplace=True)
+        x = F.leaky_relu(self.bn2(self.conv2(x)), 0.1, inplace=True)
+        x = F.leaky_relu(self.bn3(self.conv3(x)), 0.1, inplace=True)
 
-		return x
+        return x
 
 class DHead(nn.Module):
-	def __init__(self):
-		super()__init__()
+    def __init__(self):
+        super()__init__()
 
-		self.conv = nn.Conv2d(1024, 1, 1)
+        self.conv = nn.Conv2d(1024, 1, 1)
 
-	def forward(self, x):
-		output = torch.sigmoid(self.conv(x))
+    def forward(self, x):
+        output = torch.sigmoid(self.conv(x))
 
-		return output
+        return output
 
 class QHead(nn.Module):
-	def __init__(self):
-		super().__init__()
+    def __init__(self):
+        super().__init__()
 
-		self.conv1 = nn.Conv2d(1024, 128, 1, bias=False)
-		self.bn1 = nn.BatchNorm2d(128)
+        self.conv1 = nn.Conv2d(1024, 128, 1, bias=False)
+        self.bn1 = nn.BatchNorm2d(128)
 
-		self.conv_disc = nn.Conv2d(128, 10, 1)
-		self.conv_mu = nn.Conv2d(128, 2, 1)
-		self.conv_var = nn.Conv2d(128, 2, 1)
+        self.conv_disc = nn.Conv2d(128, 10, 1)
+        self.conv_mu = nn.Conv2d(128, 2, 1)
+        self.conv_var = nn.Conv2d(128, 2, 1)
 
-	def forward(self, x):
-		x = F.leaky_relu(self.bn1(self.conv1(x)), 0.1, inplace=True)
+    def forward(self, x):
+        x = F.leaky_relu(self.bn1(self.conv1(x)), 0.1, inplace=True)
 
-		disc_logits = self.conv_disc(x).squeeze()
+        disc_logits = self.conv_disc(x).squeeze()
 
-		mu = self.conv_mu(x).squeeze()
-		var = torch.exp(self.conv_var(x).squeeze())
+        mu = self.conv_mu(x).squeeze()
+        var = torch.exp(self.conv_var(x).squeeze())
 
-		return disc_logits, mu, var
+        return disc_logits, mu, var
